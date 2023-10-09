0fe2d329049bc68e5b6a7c57ae46987984b797bc
ArturoBarrios616 <A01732697@tec.com>
2023-10-09 11:45:25 -0600

    Add test for ingresar_ecuacion

--- cuentas/tests.py
+++ cuentas/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
-from cuentas.models import CustomUser
+from cuentas.models import CustomUser,Ecuacion
+from sympy import symbols, Eq, solve
 
 class CustomUserTests(TestCase):
     def setUp(self):
@@ -40,6 +41,38 @@ class CustomUserTests(TestCase):
         
         # Supongamos que, después de un inicio de sesión exitoso, redirigimos al usuario a la página de inicio o dashboard.
         self.assertRedirects(response, reverse('bienvenido'))
+
+class EcuacionTests(TestCase):
+    def setUp(self):
+        self.ingresar_ecuacion_url = reverse('ingresar_ecuacion')  
+
+    def test_agregar_multiplicacion_implicita(self):
+        # Este test no está relacionado con una vista, pero es útil para garantizar que tu función de utilidad funcione
+        from .views import agregar_multiplicacion_implicita  # Importa la función desde tu archivo de vistas
+        self.assertEqual(agregar_multiplicacion_implicita("3x"), "3*x")
+        self.assertEqual(agregar_multiplicacion_implicita("5y+3x"), "5*y+3*x")
+
+    def test_ingresar_ecuacion_POST_valid(self):
+        response = self.client.post(self.ingresar_ecuacion_url, {'ecuacion': 'x+1'})
+
+        x = symbols('x')
+        sol = solve(Eq(x+1, 0), x)
+        expected_message = "Ecuación resuelta exitosamente!"
+
+        self.assertContains(response, expected_message)
+
+    def test_ingresar_ecuacion_POST_invalid(self):
+        response = self.client.post(self.ingresar_ecuacion_url, {'ecuacion': 'x++1'})
+
+        expected_error = "No se pudo resolver la ecuación."
+
+        self.assertContains(response, expected_error)
+
+    def test_ingresar_ecuacion_GET(self):
+        response = self.client.get(self.ingresar_ecuacion_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'ingresar_ecuacion.html')
 
         
         
